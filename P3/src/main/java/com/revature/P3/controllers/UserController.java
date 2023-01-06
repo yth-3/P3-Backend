a43ab5226d9a7e5f7516b39448e5708669867790
Paul Franklin <paul219@revature.net>
2023-01-06 17:14:26 -0500

    UserController viewAllUsers exception handling

--- P3/src/main/java/com/revature/P3/controllers/UserController.java
+++ P3/src/main/java/com/revature/P3/controllers/UserController.java
@@ -1,9 +1,14 @@
 package com.revature.P3.controllers;
 
 import com.revature.P3.dtos.requests.NewUserRequest;
+import com.revature.P3.entities.User;
+import com.revature.P3.utils.custom_exceptions.InvalidAuthException;
 import com.revature.P3.utils.custom_exceptions.InvalidUserException;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
+
+import javax.servlet.http.HttpServletRequest;
+import java.util.List;
 
 @RestController
 @RequestMapping("/users")
@@ -14,9 +19,24 @@ public class UserController {
         throw new InvalidUserException();
     }
 
+    @GetMapping
+    @ResponseStatus(HttpStatus.ACCEPTED)
+    public List<User> viewAllUsers(HttpServletRequest req) {
+        String token = req.getHeader("authorization");
+        if (token == null || token.isEmpty()) throw new InvalidAuthException();
+
+        throw new InvalidAuthException();
+    }
+
     @ResponseStatus(HttpStatus.FORBIDDEN)
     @ExceptionHandler(InvalidUserException.class)
     public InvalidUserException handleInvalidUserException (InvalidUserException exception) {
+        return exception;
+    }
+
+    @ResponseStatus(HttpStatus.UNAUTHORIZED)
+    @ExceptionHandler(InvalidAuthException.class)
+    public InvalidAuthException handleInvalidAuthException (InvalidAuthException exception) {
         return exception;
     }
 }
