cc48b593809531a9b68d5de50c8cc2c5cf8d018d
Yuan Cao <yuan876@revature.net>
2023-01-06 11:31:41 -0500

    add tests for password edge cases

--- P3/src/test/java/com/revature/P3/services/HashServiceTest.java
+++ P3/src/test/java/com/revature/P3/services/HashServiceTest.java
@@ -7,18 +7,34 @@ import static org.junit.Assert.*;
 public class HashServiceTest {
     private HashService sut;
     private String password = "passw0rd";
+    private String longPassword = ";ashjd;f89v7aol;werkl/avn;asyoa;oesaosidhajkl;vn;ljkxvesif;asjf";
+    private String shortPassword = "";
+    private String nullPassword = null;
 
     @Test
     public void test_simpleHashTest_hash() {
         String hashed = sut.hash(password);
+        String hashedLongPassword = sut.hash(longPassword);
+        String hashedShortPassword = sut.hash(shortPassword);
         assertFalse(password == hashed);
+        assertFalse(longPassword == hashedLongPassword);
+        assertFalse(shortPassword == hashedShortPassword);
     }
 
     @Test
     public void test_verifiableHashTest_hash() {
         String hashed1 = sut.hash(password);
         String hashed2 = sut.hash(password);
+        String hashed3 = sut.hash(longPassword);
+        String hashed4 = sut.hash(shortPassword);
         assertTrue(sut.verify(hashed1, password));
         assertTrue(sut.verify(hashed2, password));
+        assertTrue(sut.verify(hashed3, longPassword));
+        assertTrue(sut.verify(hashed4, shortPassword));
+    }
+
+    @Test
+    public void test_dealingWithNullPassword_hash() {
+        assertThrows(NullPointerException.class, () -> sut.hash(nullPassword));
     }
 }
