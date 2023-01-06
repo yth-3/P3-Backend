2699ad42574f7e94c35b203a7fc2b121d747ac12
Yuan Cao <yuan876@revature.net>
2023-01-06 11:22:09 -0500

    reimplement hash and implement verify to pass test

--- P3/src/main/java/com/revature/P3/services/HashService.java
+++ P3/src/main/java/com/revature/P3/services/HashService.java
@@ -1,7 +1,17 @@
 package com.revature.P3.services;
 
+import de.mkammerer.argon2.Argon2;
+import de.mkammerer.argon2.Argon2Factory;
+
 public class HashService {
+    private static Argon2 argon2 = Argon2Factory.create();
+
     public static String hash(String plain) {
-        return null;
+        String hash = argon2.hash(10, 65536, 1, plain.toCharArray());
+        return hash;
+    }
+
+    public static boolean verify(String hash, String password) {
+        return argon2.verify(hash, password);
     }
 }
