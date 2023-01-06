e268a4e0858b86aa6798dbc63610d88eb0732e90
Paul Franklin <paul219@revature.net>
2023-01-06 18:58:25 -0500

    ClaimController viewAllClaims with default exceptions

--- P3/src/main/java/com/revature/P3/controllers/ClaimController.java
+++ P3/src/main/java/com/revature/P3/controllers/ClaimController.java
@@ -1,11 +1,29 @@
 package com.revature.P3.controllers;
 
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.revature.P3.entities.Claim;
+import com.revature.P3.utils.custom_exceptions.InvalidAuthException;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+import javax.servlet.http.HttpServletRequest;
+import java.util.List;
 
 @CrossOrigin
 @RestController
 @RequestMapping("/claims")
 public class ClaimController {
+    @GetMapping
+    @ResponseStatus(HttpStatus.ACCEPTED)
+    public List<Claim> viewAllClaims(HttpServletRequest req) {
+        String token = req.getHeader("authorization");
+        if (token == null || token.isEmpty()) throw new InvalidAuthException();
+
+        throw new InvalidAuthException();
+    }
+
+    @ResponseStatus(HttpStatus.UNAUTHORIZED)
+    @ExceptionHandler(InvalidAuthException.class)
+    public InvalidAuthException handleInvalidAuthException (InvalidAuthException exception) {
+        return exception;
+    }
 }
