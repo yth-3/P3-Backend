998eef2ec2d0bf73dd1214173c3a0e593320b866
Paul Franklin <paul219@revature.net>
2023-01-09 15:07:43 -0500

    TDD methods

--- P3/src/test/java/com/revature/P3/services/TokenServiceTest.java
+++ P3/src/test/java/com/revature/P3/services/TokenServiceTest.java
@@ -1,4 +1,34 @@
 package com.revature.P3.services;
 
+import com.revature.P3.utils.JwtConfig;
+import org.junit.Before;
+import org.junit.Test;
+import org.mockito.Mockito;
+
 public class TokenServiceTest {
+    private TokenService sut;
+    private final JwtConfig mockJwtConfig = Mockito.mock(JwtConfig.class);
+
+    @Before
+    public void init() {
+        sut = new TokenService(mockJwtConfig);
+    }
+
+    @Test
+    public void test_createNewToken_givenPrincipalSubject() {
+        // Arrange
+
+        // Act
+
+        // Assert
+    }
+
+    @Test
+    public void test_retrievePrincipalFromToken_givenToken() {
+        // Arrange
+
+        // Act
+
+        // Assert
+    }
 }
