22e1462fdf0d5d22b3725564f08404e2148dd34c
Paul Franklin <paul219@revature.net>
2023-01-09 11:41:56 -0500

    UserController createUser

--- P3/src/main/java/com/revature/P3/services/UserService.java
+++ P3/src/main/java/com/revature/P3/services/UserService.java
@@ -1,6 +1,7 @@
 package com.revature.P3.services;
 
 import com.revature.P3.dtos.requests.NewLoginRequest;
+import com.revature.P3.dtos.requests.NewUserRequest;
 import com.revature.P3.dtos.responses.Principal;
 import org.springframework.stereotype.Service;
 
@@ -8,5 +9,8 @@ import org.springframework.stereotype.Service;
 public class UserService {
     public Principal loginUser(NewLoginRequest req) {
         return null;
+    }
+
+    public void createUser(NewUserRequest req) {
     }
 }
