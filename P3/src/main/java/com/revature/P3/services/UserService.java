091924c9b2d24e2cc9dd86bdaefc34278fa769d9
Yuan Cao <yuan876@revature.net>
2023-01-09 14:34:59 -0500

    added constructor

--- P3/src/main/java/com/revature/P3/services/UserService.java
+++ P3/src/main/java/com/revature/P3/services/UserService.java
@@ -2,10 +2,17 @@ package com.revature.P3.services;
 
 import com.revature.P3.dtos.requests.NewLoginRequest;
 import com.revature.P3.dtos.responses.Principal;
+import com.revature.P3.repositories.UserRepository;
 import org.springframework.stereotype.Service;
 
 @Service
 public class UserService {
+    private final UserRepository userRepo;
+
+    public UserService(UserRepository userRepo) {
+        this.userRepo = userRepo;
+    }
+
     public Principal loginUser(NewLoginRequest req) {
         return null;
     }
