850cf2b8d0d57888b49ce6a3e852f28fe7e157ea
Paul Franklin <paul219@revature.net>
2023-01-10 14:40:24 -0500

    Update dml.sql

--- P3/src/main/sql/dml.sql
+++ P3/src/main/sql/dml.sql
@@ -9,6 +9,6 @@ VALUES('0', 'Patient'),
 INSERT INTO users
 (user_id,username,password,email,registered,is_active,role_id)
 VALUES('admin','admin','$argon2i$v=19$m=65536,t=10,p=1$V801axe5AYOQ7Ur1jiJgsw$AOqsED3bbYs5ZTv+Ub9okVEv03q3nnVWq6m/RcmwUDs','admin@admin.com','2022-01-10 00:00:00',true,'4'),
-('insurer','insurer','insurer','insurer@insurer.com','2022-01-10 00:00:00',true,'3'),
-('doctor','doctor','doctor','doctor@doctor.com','2022-01-10 00:00:00',true,'2'),
-('nurse','nurse','nurse','nurse@nurse.com','2022-01-10 00:00:00',true,'1');
+('insurer','insurer','$argon2i$v=19$m=65536,t=10,p=1$V801axe5AYOQ7Ur1jiJgsw$AOqsED3bbYs5ZTv+Ub9okVEv03q3nnVWq6m/RcmwUDs','insurer@insurer.com','2022-01-10 00:00:00',true,'3'),
+('doctor','doctor','$argon2i$v=19$m=65536,t=10,p=1$V801axe5AYOQ7Ur1jiJgsw$AOqsED3bbYs5ZTv+Ub9okVEv03q3nnVWq6m/RcmwUDs','doctor@doctor.com','2022-01-10 00:00:00',true,'2'),
+('nurse','nurse','$argon2i$v=19$m=65536,t=10,p=1$V801axe5AYOQ7Ur1jiJgsw$AOqsED3bbYs5ZTv+Ub9okVEv03q3nnVWq6m/RcmwUDs','nurse@nurse.com','2022-01-10 00:00:00',true,'1');
