460004152a702058a8f520b53f0be6d7fd7e68c1
Paul Franklin <paul219@revature.net>
2023-01-10 10:43:06 -0500

    Admin, insurer and staff accounts

--- P3/src/main/sql/dml.sql
+++ P3/src/main/sql/dml.sql
@@ -5,3 +5,10 @@ VALUES('0', 'Patient'),
 ('2', 'Doctor'),
 ('3', 'Insurer'),
 ('4', 'Admin');
+
+INSERT INTO users
+(user_id,username,password,email,registered,is_active,role_id)
+VALUES('admin','admin','admin','admin@admin.com','2022-01-10 00:00:00',true,'4'),
+('insurer','insurer','insurer','insurer@insurer.com','2022-01-10 00:00:00',true,'3'),
+('doctor','doctor','doctor','doctor@doctor.com','2022-01-10 00:00:00',true,'2'),
+('nurse','nurse','nurse','nurse@nurse.com','2022-01-10 00:00:00',true,'1');
