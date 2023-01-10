f6b8bd20198647012c2254dad72f6140298b4b68
Paul Franklin <paul219@revature.net>
2023-01-09 19:04:54 -0500

    Update HashService.java

--- P3/src/main/java/com/revature/P3/services/HashService.java
+++ P3/src/main/java/com/revature/P3/services/HashService.java
@@ -2,7 +2,9 @@ package com.revature.P3.services;
 
 import de.mkammerer.argon2.Argon2;
 import de.mkammerer.argon2.Argon2Factory;
+import org.springframework.stereotype.Service;
 
+@Service
 public class HashService {
     private final static Argon2 argon2 = Argon2Factory.create();
 
