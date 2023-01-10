924ab70c3dd47b00f146d12ecf6485421842ea91
Yuan Cao <yuan876@revature.net>
2023-01-10 17:35:35 -0500

    add NewClaimRequest implementation

--- P3/src/main/java/com/revature/P3/dtos/requests/NewClaimRequest.java
+++ P3/src/main/java/com/revature/P3/dtos/requests/NewClaimRequest.java
@@ -1,4 +1,50 @@
 package com.revature.P3.dtos.requests;
 
 public class NewClaimRequest {
+    private double claimedAmount;
+    private String claimType;
+    private String description;
+
+    public NewClaimRequest() {
+        super();
+    }
+
+    public NewClaimRequest(double claimedAmount, String claimType, String description) {
+        this.claimedAmount = claimedAmount;
+        this.claimType = claimType;
+        this.description = description;
+    }
+
+    public double getClaimedAmount() {
+        return claimedAmount;
+    }
+
+    public void setClaimedAmount(double claimedAmount) {
+        this.claimedAmount = claimedAmount;
+    }
+
+    public String getClaimType() {
+        return claimType;
+    }
+
+    public void setClaimType(String claimType) {
+        this.claimType = claimType;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    @Override
+    public String toString() {
+        return "NewClaimRequest{" +
+                "claimedAmount=" + claimedAmount +
+                ", claimType='" + claimType + '\'' +
+                ", description='" + description + '\'' +
+                '}';
+    }
 }
