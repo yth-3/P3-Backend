17a271f7fb904a196caa0edb96e3343486f5e564
Yuan Cao <yuan876@revature.net>
2023-01-10 16:09:44 -0500

    added conconstructor for specifying an error message

--- P3/src/main/java/com/revature/P3/utils/custom_exceptions/InvalidClaimException.java
+++ P3/src/main/java/com/revature/P3/utils/custom_exceptions/InvalidClaimException.java
@@ -1,4 +1,7 @@
 package com.revature.P3.utils.custom_exceptions;
 
 public class InvalidClaimException extends RuntimeException {
+    public InvalidClaimException(String message) {
+        super(message);
+    }
 }
