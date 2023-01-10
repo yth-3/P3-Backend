f532a1cebceff3d4004a05aef4a8204a3eb36110
Paul Franklin <paul219@revature.net>
2023-01-10 12:10:02 -0500

    Update dml.sql

--- P3/src/main/sql/dml.sql
+++ P3/src/main/sql/dml.sql
@@ -8,7 +8,7 @@ VALUES('0', 'Patient'),
 
 INSERT INTO users
 (user_id,username,password,email,registered,is_active,role_id)
-VALUES('admin','admin','admin','admin@admin.com','2022-01-10 00:00:00',true,'4'),
+VALUES('admin','admin','$argon2i$v=19$m=65536,t=10,p=1$V801axe5AYOQ7Ur1jiJgsw$AOqsED3bbYs5ZTv+Ub9okVEv03q3nnVWq6m/RcmwUDs','admin@admin.com','2022-01-10 00:00:00',true,'4'),
 ('insurer','insurer','insurer','insurer@insurer.com','2022-01-10 00:00:00',true,'3'),
 ('doctor','doctor','doctor','doctor@doctor.com','2022-01-10 00:00:00',true,'2'),
 ('nurse','nurse','nurse','nurse@nurse.com','2022-01-10 00:00:00',true,'1');
