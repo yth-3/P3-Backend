bf7b1a507ea458198954c20192dd1bfaf4ce9589
Paul Franklin <paul219@revature.net>
2023-01-11 15:58:53 -0500

    Login signup #42

--- P3/src/test/java/com/revature/P3/services/ClaimServiceTest.java
+++ P3/src/test/java/com/revature/P3/services/ClaimServiceTest.java
@@ -1,17 +1,26 @@
 package com.revature.P3.services;
 
+import com.revature.P3.dtos.requests.NewClaimRequest;
+import com.revature.P3.dtos.responses.Principal;
 import com.revature.P3.repositories.ClaimRepository;
 import org.junit.Before;
 import org.junit.Test;
 import org.mockito.Mockito;
 
 public class ClaimServiceTest {
-    private ClaimService sut;
     private final ClaimRepository mockClaimRepo = Mockito.mock(ClaimRepository.class);
+
+    private ClaimService sut;
+    private NewClaimRequest validClaimReq;
+    private Principal validPrincipal;
 
     @Before
     public void init() {
         sut = new ClaimService(mockClaimRepo);
+        validClaimReq = new NewClaimRequest(10.00,
+                "PreventativeCare",
+                "Went to see the doc for annual physical");
+        validPrincipal = new Principal("userId", "username", "email", "registered", true, "Patient");
     }
 
     @Test
