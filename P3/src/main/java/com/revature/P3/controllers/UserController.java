355fc68bd081ad4b7f9800ddbf75f7cc65e465e7
Yuan Cao <yuan876@revature.net>
2023-01-11 15:11:26 -0500

    refactor with better error detection and flow

--- P3/src/main/java/com/revature/P3/controllers/UserController.java
+++ P3/src/main/java/com/revature/P3/controllers/UserController.java
@@ -6,6 +6,7 @@ import com.revature.P3.enums.Roles;
 import com.revature.P3.services.HashService;
 import com.revature.P3.services.TokenService;
 import com.revature.P3.services.UserService;
+import com.revature.P3.utils.custom_exceptions.BadGatewayException;
 import com.revature.P3.utils.custom_exceptions.InvalidAuthException;
 import com.revature.P3.utils.custom_exceptions.InvalidUserException;
 import org.springframework.http.HttpStatus;
@@ -29,71 +30,14 @@ public class UserController {
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
     public void createPatient(@RequestBody(required = false) NewUserRequest req) {
+        if (req == null || req.getUsername() == null || req.getPassword() == null || req.getEmail() == null) {
+            throw new InvalidUserException("Invalid signup request or duplicate user found");
+        }
+
+        req.setPassword(HashService.getHash(req.getPassword()));
         try {
-            req.setPassword(HashService.getHash(req.getPassword()));
             userService.createPatient(req);
-        }
-        catch (InvalidUserException exception) {
-            throw exception;
-        }
-    }
-
-    @PostMapping(path="nurse")
-    @ResponseStatus(HttpStatus.CREATED)
-    public void createNurse(@RequestBody(required = false) NewUserRequest req, HttpServletRequest servletReq) {
-        String token = servletReq.getHeader("authorization");
-        if (token == null || token.isEmpty()) throw new InvalidAuthException("Not Authorized");
-
-        Principal principal = tokenService.retrievePrincipalFromToken(token);
-        String role = principal.getRole();
-
-        if (!role.equals(Roles.Admin.toString())) throw new InvalidAuthException("Not Authorized");
-
-        try {
-            req.setPassword(HashService.getHash(req.getPassword()));
-            userService.createNurse(req);
-        }
-        catch (InvalidUserException exception) {
-            throw exception;
-        }
-    }
-
-    @PostMapping(path="doctor")
-    @ResponseStatus(HttpStatus.CREATED)
-    public void createDoctor(@RequestBody(required = false) NewUserRequest req, HttpServletRequest servletReq) {
-        String token = servletReq.getHeader("authorization");
-        if (token == null || token.isEmpty()) throw new InvalidAuthException("Not Authorized");
-
-        Principal principal = tokenService.retrievePrincipalFromToken(token);
-        String role = principal.getRole();
-
-        if (!role.equals(Roles.Admin.toString())) throw new InvalidAuthException("Not Authorized");
-
-        try {
-            req.setPassword(HashService.getHash(req.getPassword()));
-            userService.createDoctor(req);
-        }
-        catch (InvalidUserException exception) {
-            throw exception;
-        }
-    }
-
-    @PostMapping(path="insurer")
-    @ResponseStatus(HttpStatus.CREATED)
-    public void createInsurer(@RequestBody(required = false) NewUserRequest req, HttpServletRequest servletReq) {
-        String token = servletReq.getHeader("authorization");
-        if (token == null || token.isEmpty()) throw new InvalidAuthException("Not Authorized");
-
-        Principal principal = tokenService.retrievePrincipalFromToken(token);
-        String role = principal.getRole();
-
-        if (!role.equals(Roles.Admin.toString())) throw new InvalidAuthException("Not Authorized");
-
-        try {
-            req.setPassword(HashService.getHash(req.getPassword()));
-            userService.createInsurer(req);
-        }
-        catch (InvalidUserException exception) {
+        } catch (Exception exception) {
             throw exception;
         }
     }
@@ -102,18 +46,90 @@ public class UserController {
     @ResponseStatus(HttpStatus.CREATED)
     public void createStaff(@RequestBody(required = false) NewUserRequest req, HttpServletRequest servletReq) {
         String token = servletReq.getHeader("authorization");
-        if (token == null || token.isEmpty()) throw new InvalidAuthException("Not Authorized");
+        if (token == null || token.isEmpty()) {
+            throw new InvalidAuthException("Not Authorized");
+        }
 
         Principal principal = tokenService.retrievePrincipalFromToken(token);
         String role = principal.getRole();
-
-        if (!role.equals(Roles.Admin.toString())) throw new InvalidAuthException("Not Authorized");
-
-        try {
-            req.setPassword(HashService.getHash(req.getPassword()));
-            userService.createStaff(req);
+        if (!role.equals(Roles.Admin.toString())) {
+            throw new InvalidAuthException("Not Authorized");
         }
-        catch (InvalidUserException exception) {
+
+        if (req == null || req.getUsername() == null || req.getPassword() == null || req.getEmail() == null) {
+            throw new InvalidUserException("Invalid signup request or duplicate user found");
+        }
+
+        req.setPassword(HashService.getHash(req.getPassword()));
+        try {
+            userService.createStaff(req);
+        } catch (InvalidUserException exception) {
+            throw exception;
+        }
+    }
+
+    @PostMapping(path="insurer")
+    @ResponseStatus(HttpStatus.CREATED)
+    public void createInsurer(@RequestBody(required = false) NewUserRequest req, HttpServletRequest servletReq) {
+        String token = servletReq.getHeader("authorization");
+        if (token == null || token.isEmpty()) {
+            throw new InvalidAuthException("Not Authorized");
+        }
+
+        Principal principal = tokenService.retrievePrincipalFromToken(token);
+        String role = principal.getRole();
+        if (!role.equals(Roles.Admin.toString())) {
+            throw new InvalidAuthException("Not Authorized");
+        }
+
+        req.setPassword(HashService.getHash(req.getPassword()));
+        try {
+            userService.createInsurer(req);
+        } catch (InvalidUserException exception) {
+            throw exception;
+        }
+    }
+
+    @PostMapping(path="doctor")
+    @ResponseStatus(HttpStatus.CREATED)
+    public void createDoctor(@RequestBody(required = false) NewUserRequest req, HttpServletRequest servletReq) {
+        String token = servletReq.getHeader("authorization");
+        if (token == null || token.isEmpty()) {
+            throw new InvalidAuthException("Not Authorized");
+        }
+
+        Principal principal = tokenService.retrievePrincipalFromToken(token);
+        String role = principal.getRole();
+        if (!role.equals(Roles.Admin.toString())) {
+            throw new InvalidAuthException("Not Authorized");
+        }
+
+        req.setPassword(HashService.getHash(req.getPassword()));
+        try {
+            userService.createDoctor(req);
+        } catch (InvalidUserException exception) {
+            throw exception;
+        }
+    }
+
+    @PostMapping(path="nurse")
+    @ResponseStatus(HttpStatus.CREATED)
+    public void createNurse(@RequestBody(required = false) NewUserRequest req, HttpServletRequest servletReq) {
+        String token = servletReq.getHeader("authorization");
+        if (token == null || token.isEmpty()) {
+            throw new InvalidAuthException("Not Authorized");
+        }
+
+        Principal principal = tokenService.retrievePrincipalFromToken(token);
+        String role = principal.getRole();
+        if (!role.equals(Roles.Admin.toString())) {
+            throw new InvalidAuthException("Not Authorized");
+        }
+
+        req.setPassword(HashService.getHash(req.getPassword()));
+        try {
+            userService.createNurse(req);
+        } catch (InvalidUserException exception) {
             throw exception;
         }
     }
@@ -163,6 +179,12 @@ public class UserController {
     @ResponseStatus(HttpStatus.FORBIDDEN)
     @ExceptionHandler(InvalidUserException.class)
     public String handleInvalidUserException (InvalidUserException exception) {
+        return exception.getMessage();
+    }
+
+    @ResponseStatus(HttpStatus.BAD_GATEWAY)
+    @ExceptionHandler(BadGatewayException.class)
+    public String handleBadGatewayException (BadGatewayException exception) {
         return exception.getMessage();
     }
 
