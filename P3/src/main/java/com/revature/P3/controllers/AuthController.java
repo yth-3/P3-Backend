197cd3a8e5c80c2e249b2589f2df7177333c5622
Yuan Cao <yuan876@revature.net>
2023-01-11 11:55:09 -0500

    refactor login to check edge cases better and have better error message and have ErrorHandlers return error message instead of errors

--- P3/src/main/java/com/revature/P3/controllers/AuthController.java
+++ P3/src/main/java/com/revature/P3/controllers/AuthController.java
@@ -6,6 +6,7 @@ import com.revature.P3.entities.User;
 import com.revature.P3.services.HashService;
 import com.revature.P3.services.TokenService;
 import com.revature.P3.services.UserService;
+import com.revature.P3.utils.custom_exceptions.BadGatewayException;
 import com.revature.P3.utils.custom_exceptions.InvalidAuthException;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
@@ -25,29 +26,33 @@ public class AuthController {
     @PostMapping
     @ResponseStatus(HttpStatus.ACCEPTED)
     public Principal login(@RequestBody(required = false) NewLoginRequest req) {
-        if (req == null) {
-            throw new InvalidAuthException("Not Authorized");
+        if (req == null || req.getUsername() == null || req.getPassword() == null) {
+            throw new InvalidAuthException("Invalid Request");
+        }
+
+        User candidate = null;
+        try {
+            candidate = userService.loginUser(req);;
+        } catch (BadGatewayException e) {
+            throw e;
+        }
+
+        if (candidate == null) {
+            throw new InvalidAuthException("Invalid Username or Password");
         }
 
         Principal principal = null;
-        try {
-            User candidate = userService.loginUser(req);
-
-            if (HashService.verify(candidate.getPassword(), req.getPassword())) {
-                principal = new Principal(
-                        candidate.getUserId(),
-                        candidate.getUsername(),
-                        candidate.getEmail(),
-                        candidate.getRegistered().toString(),
-                        candidate.getActive(),
-                        candidate.getRole().getRole()
-                );
-            }
-            else {
-                throw new InvalidAuthException("Not Authorized");
-            }
-        } catch (Exception exception) {
-            throw new InvalidAuthException("Not Authorized");
+        if (HashService.verify(candidate.getPassword(), req.getPassword())) {
+            principal = new Principal(
+                    candidate.getUserId(),
+                    candidate.getUsername(),
+                    candidate.getEmail(),
+                    candidate.getRegistered().toString(),
+                    candidate.getActive(),
+                    candidate.getRole().getRole()
+            );
+        } else {
+            throw new InvalidAuthException("Invalid Username or Password");
         }
 
         String token = tokenService.createNewToken(principal);
@@ -58,6 +63,12 @@ public class AuthController {
     @ResponseStatus(HttpStatus.UNAUTHORIZED)
     @ExceptionHandler(InvalidAuthException.class)
     public String handleInvalidAuthException (InvalidAuthException exception) {
+        return exception.getMessage();
+    }
+
+    @ResponseStatus(HttpStatus.BAD_GATEWAY)
+    @ExceptionHandler(Exception.class)
+    public String handleBadGatewayException (BadGatewayException exception) {
         return exception.getMessage();
     }
 }
