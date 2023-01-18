240e33a3e21f7dad027a4bd3b9e6d5ebff3c11dc
Paul Franklin <paul219@revature.net>
2023-01-18 14:38:24 -0500

    Update NewClaimReviewRequest.java

--- P3/src/main/java/com/revature/P3/dtos/requests/NewClaimReviewRequest.java
+++ P3/src/main/java/com/revature/P3/dtos/requests/NewClaimReviewRequest.java
@@ -3,6 +3,10 @@ package com.revature.P3.dtos.requests;
 public class NewClaimReviewRequest {
     private Double settled;
 
+    public NewClaimReviewRequest() {
+        super();
+    }
+
     public NewClaimReviewRequest(Double settled) {
         this.settled = settled;
     }
